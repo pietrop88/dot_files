c74978b70811b6aecd2d6eb8c92102e372d1d0a3
Pietro Panizza <pietropanizza88@gmail.com>
2022-08-12 10:01:20 +0200

    Add icon for battery charging on waybar.

--- .config/waybar/scripts/battery_icon.sh
+++ .config/waybar/scripts/battery_icon.sh
@@ -1,24 +1,8 @@
 #!/bin/sh
 
-VAL=$(cat /sys/class/power_supply/BAT0/capacity)
-if [ $VAL -gt 90 ]; then 
+VAL=$(cat /sys/class/power_supply/BAT0/status)
+if [ $VAL = "Charging" ]; then 
+  echo ""
+else
   echo ""
-elif [ $VAL -gt 80 ]; then
-  echo ""
-elif [ $VAL -gt 70 ]; then
-  echo ""
-elif [ $VAL -gt 60 ]; then
-  echo ""
-elif [ $VAL -gt 50 ]; then
-  echo ""
-elif [ $VAL -gt 40 ]; then
-  echo ""
-elif [ $VAL -gt 30 ]; then
-  echo ""
-elif [ $VAL -gt 20]; then
-  echo ""
-elif [ $VAL -gt 10]; then
-  echo ""
-else 
-  echo ""
 fi
