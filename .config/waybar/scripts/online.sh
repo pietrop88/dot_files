978001c98d780a7368339250eb928086a71e6dc6
Pietro Panizza <pietropanizza88@gmail.com>
2023-12-27 23:39:42 +0100

    Update battery on full and transcode online interface.

--- .config/waybar/scripts/online.sh
+++ .config/waybar/scripts/online.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
+WIFI='wlp59s0'
+USB='enp0s20f0u2'
+
 st=$(ip route get 8.8.8.8)
 if [ $? -eq 0 ]; then
   iface=$(echo $st | sed -n 's/.*dev \([^\ ]*\).*/\1/p')
+  if [ "$iface" = "$WIFI" ]; then 
+    iface='WIFI' 
+  elif [ "$iface" = "$USB" ]; then 
+    iface='USB' 
+  fi
   echo "ONLINE [$iface]"
 else
   echo 'OFFLINE'	
