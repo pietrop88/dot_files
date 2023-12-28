88f15b043924b2c89a32e85102cd9e84e5400884
Pietro Panizza <pietropanizza88@gmail.com>
2023-12-28 21:08:50 +0100

    Improve online script with ping.

--- .config/waybar/scripts/online.sh
+++ .config/waybar/scripts/online.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 WIFI='wlp59s0'
 USB='enp0s20f0u2'
+IP=8.8.8.8
 
-st=$(ip route get 8.8.8.8)
-if [ $? -eq 0 ]; then
-  iface=$(echo $st | sed -n 's/.*dev \([^\ ]*\).*/\1/p')
+if ping -c 1 $IP > /dev/null 2>&1; then
+  iface=$(ip route get $IP | sed -n 's/.*dev \([^\ ]*\).*/\1/p')
   if [ "$iface" = "$WIFI" ]; then 
     iface='WIFI' 
   elif [ "$iface" = "$USB" ]; then 
