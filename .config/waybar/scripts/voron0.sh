5f903a3575aa1bd9ea09a9b5f61fe467f6f25c68
Pietro Panizza <pietropanizza88@gmail.com>
2023-12-28 20:54:48 +0100

    Add voron0 script.

---
+++ .config/waybar/scripts/voron0.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+IP='voron0.local'
+PORT=7125
+INITSTR='VORON0'
+
+if ping -c 1 $IP > /dev/null 2>&1; then
+  tmp=$(curl -s "$IP:$PORT/printer/objects/query?print_stats&virtual_sdcard")
+  st=$(echo $tmp | jq -r '.result.status.print_stats.state')
+  if [ "$st" = "printing" ]; then
+    pr=$(echo $tmp | jq -r '.result.status.virtual_sdcard.progress | tonumber | floor')
+    echo "$INITSTR [$st - $pr%]"
+  else
+    echo "$INITSTR [$st]"
+  fi
+else
+  echo "$INITSTR [OFFLINE]"	
+fi
+
