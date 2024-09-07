4ab69c7d806a476993613eb870cd39541383f666
Pietro Panizza <pietropanizza88@gmail.com>
2024-09-07 13:06:13 +0200

    Fix voron api.

--- .config/waybar/scripts/voron0.sh
+++ .config/waybar/scripts/voron0.sh
@@ -7,7 +7,7 @@ if ping -c 1 $IP > /dev/null 2>&1; then
   tmp=$(curl -s "$IP:$PORT/printer/objects/query?print_stats&virtual_sdcard")
   st=$(echo $tmp | jq -r '.result.status.print_stats.state')
   if [ "$st" = "printing" ]; then
-    pr=$(echo $tmp | jq -r '.result.status.virtual_sdcard.progress | tonumber | floor')
+    pr=$(echo $tmp | jq -r '.result.status.virtual_sdcard.progress | tonumber | .*100 | floor') 
     echo "$INITSTR [$st - $pr%]"
   else
     echo "$INITSTR [$st]"
