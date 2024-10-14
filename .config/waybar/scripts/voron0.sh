650a837715db391e0111658a3d22f9ce522fa704
Pietro Panizza <pietropanizza88@gmail.com>
2024-10-14 23:15:16 +0200

    Fix voron0 progress in waybar.

--- .config/waybar/scripts/voron0.sh
+++ .config/waybar/scripts/voron0.sh
@@ -4,10 +4,10 @@ PORT=7125
 INITSTR='VORON0'
 
 if ping -c 1 $IP > /dev/null 2>&1; then
-  tmp=$(curl -s "$IP:$PORT/printer/objects/query?print_stats&virtual_sdcard")
+  tmp=$(curl -s "$IP:$PORT/printer/objects/query?print_stats&display_status")
   st=$(echo $tmp | jq -r '.result.status.print_stats.state')
   if [ "$st" = "printing" ]; then
-    pr=$(echo $tmp | jq -r '.result.status.virtual_sdcard.progress | tonumber | .*100 | floor') 
+    pr=$(echo $tmp | jq -r '.result.status.display_status.progress | tonumber | .*100 | floor') 
     echo "$INITSTR [$st - $pr%]"
   else
     echo "$INITSTR [$st]"
